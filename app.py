23c3eb183f32a512851adb313e78046b8794ad44
Fernando Araya Anderson <fer@aro.red>
2026-09-26 11:09:44 -0600

    Actualizar app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from openai 
-import OpenAIimport os
+from openai import OpenAI
+import os
 # 1. Configuración de la interfaz adaptativa para web y dispositivos móviles
 st.set_page_config(
     page_title="Asistente Democracia Participativa - TS 2026", 
